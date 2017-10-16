bb61641038dd80c9dfb14ee7510ab120f00b1595
beaston02 <beaston02@gmail.com>
2017-10-16 16:13:12 -0700

    Fixed for new api system used to get online models
    fixed issue #8

--- SM.py
+++ SM.py
@@ -4,33 +4,32 @@ from websocket import create_connection
 
 UserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Mobile Safari/537.36"
 class get:
-    def onlineModels(self):
+    def online_models(self):
+        r = requests.session()
         online = []
-        offline = False
         page = 1
-        while offline == False:
-            result = requests.get("https://api.naiadsystems.com/search/V1/list?skin_search_kids=0&exact=1&page_number={}&results_per_page=500&sort_order=default".format(
-                    page), headers={"User-Agent": UserAgent}).json()
+        while True:
             try:
+                result = r.get("https://streamate.com/api/search/list?skin_search_kids=0&exact=100&page_number={}&downboost_paid=true".format(
+                    page), headers={"User-Agent": UserAgent}).json()
                 for model in result['Results']:
                     if model['LiveStatus'] != 'offline':
                         if model['InExclusiveShow'] is False and model['GoldShow'] is False:
                             online.append(model['Nickname'].lower())
 
                     else:
-                        offline = True
-                        return online
-                page = page + 1
+                        return list(set(online))
+                page += 1
             except:
                 pass
 
     def stream(self, model):
-        modelinfo = requests.get("https://streamate.com/ajax/config/?name={}&sakey=&sk=streamate.com&userid=0&version=2.3.1&ajax=1".format(model)).json()
-        if modelinfo['stream']['serverId'] != '0':
+        model_info = requests.get("https://streamate.com/ajax/config/?name={}&sakey=&sk=streamate.com&userid=0&version=2.3.1&ajax=1".format(model)).json()
+        if model_info['stream']['serverId'] != '0':
 
-            URL = modelinfo['stream']['nodeHost'] + "/socket.io/?performerid=" + str(
-                modelinfo['performer']['id']) + "&sserver=" + modelinfo['stream']['serverId'] + "&streamid=" + \
-                  modelinfo['stream'][
+            URL = model_info['stream']['nodeHost'] + "/socket.io/?performerid=" + str(
+                model_info['performer']['id']) + "&sserver=" + model_info['stream']['serverId'] + "&streamid=" + \
+                  model_info['stream'][
                       'streamId'] + "&sakey=&sessiontype=preview&perfdiscountid=0&minduration=0&goldshowid=0&version=7&referrer=hybrid.client.2.3.1%2Favchat.swf&usertype=false&EIO=3&transport=websocket"
             ws = create_connection(URL)
             i = 0
@@ -42,14 +41,14 @@ class get:
                     roomInfo = result['data'][22]
                     videourl = ('https:' + requests.utils.quote(
                         '//sea1b-ls.naiadsystems.com/sea1b-hub-api/8101/videourl') + '?' + requests.utils.quote(
-                        'payload') + '=' + requests.utils.quote('{"puserid":"' + str(modelinfo['performer']['id']) + '","roomid":"' + roomInfo[
+                        'payload') + '=' + requests.utils.quote('{"puserid":"' + str(model_info['performer']['id']) + '","roomid":"' + roomInfo[
                             'roomid'] + '","showtype":1,"nginx":1}'))
                     videoinfo = requests.get(videourl).json()
                     videoinfo = requests.get(videoinfo[0]['url']).json()
                     return videoinfo['formats']['mp4-hls']['manifest']
         else: return 'Offline'
 
-    def modelApiCheck(self, model):
+    def model_api_check(self, model):
         result = requests.get('https://streamate.com/search.php?q={}&sssjson=1'.format(model),
                               headers={"User-Agent": UserAgent}).json()['Results']
         if model.lower() not in [m['Nickname'].lower() for m in result]:
