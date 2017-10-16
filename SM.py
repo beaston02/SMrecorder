78a05d09146e158de05020dbddf09d62cb5846c9
beaston02 <beaston02@gmail.com>
2017-10-16 16:39:54 -0700

    Update SM.py

--- SM.py
+++ SM.py
@@ -26,30 +26,30 @@ class get:
     def stream(self, model):
         try:
             model_info = requests.get("https://streamate.com/ajax/config/?name={}&sakey=&sk=streamate.com&userid=0&version=2.3.1&ajax=1".format(model)).json()
-        except:
-            return('Offline')
-        if model_info['stream']['serverId'] != '0':
+            if model_info['stream']['serverId'] != '0':
 
-            URL = model_info['stream']['nodeHost'] + "/socket.io/?performerid=" + str(
-                model_info['performer']['id']) + "&sserver=" + model_info['stream']['serverId'] + "&streamid=" + \
-                  model_info['stream'][
-                      'streamId'] + "&sakey=&sessiontype=preview&perfdiscountid=0&minduration=0&goldshowid=0&version=7&referrer=hybrid.client.2.3.1%2Favchat.swf&usertype=false&EIO=3&transport=websocket"
-            ws = create_connection(URL)
-            i = 0
-            while i < 5:
-                result = ws.recv()
-                i = i + 1
-                if "roomid" in result:
-                    result = json.loads(result[2:])[1]
-                    roomInfo = result['data'][22]
-                    videourl = ('https:' + requests.utils.quote(
-                        '//sea1b-ls.naiadsystems.com/sea1b-hub-api/8101/videourl') + '?' + requests.utils.quote(
-                        'payload') + '=' + requests.utils.quote('{"puserid":"' + str(model_info['performer']['id']) + '","roomid":"' + roomInfo[
-                            'roomid'] + '","showtype":1,"nginx":1}'))
-                    videoinfo = requests.get(videourl).json()
-                    videoinfo = requests.get(videoinfo[0]['url']).json()
-                    return videoinfo['formats']['mp4-hls']['manifest']
-        else: return 'Offline'
+                URL = model_info['stream']['nodeHost'] + "/socket.io/?performerid=" + str(
+                    model_info['performer']['id']) + "&sserver=" + model_info['stream']['serverId'] + "&streamid=" + \
+                      model_info['stream'][
+                          'streamId'] + "&sakey=&sessiontype=preview&perfdiscountid=0&minduration=0&goldshowid=0&version=7&referrer=hybrid.client.2.3.1%2Favchat.swf&usertype=false&EIO=3&transport=websocket"
+                ws = create_connection(URL)
+                i = 0
+                while i < 5:
+                    result = ws.recv()
+                    i = i + 1
+                    if "roomid" in result:
+                        result = json.loads(result[2:])[1]
+                        roomInfo = result['data'][22]
+                        videourl = ('https:' + requests.utils.quote(
+                            '//sea1b-ls.naiadsystems.com/sea1b-hub-api/8101/videourl') + '?' + requests.utils.quote(
+                            'payload') + '=' + requests.utils.quote('{"puserid":"' + str(model_info['performer']['id']) + '","roomid":"' + roomInfo[
+                                'roomid'] + '","showtype":1,"nginx":1}'))
+                        videoinfo = requests.get(videourl).json()
+                        videoinfo = requests.get(videoinfo[0]['url']).json()
+                        return videoinfo['formats']['mp4-hls']['manifest']
+            else: return 'Offline'
+        except:
+            return ('Offline')
 
     def model_api_check(self, model):
         result = requests.get('https://streamate.com/search.php?q={}&sssjson=1'.format(model),
